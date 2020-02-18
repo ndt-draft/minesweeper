f571e4ad60157cf1df81cd05709e06cef8bf400e
thanh4890 <thanh4890@gmail.com>
2020-02-18 14:10:52 +0700

    make hint and handle click button

--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const Button = props => (
-  <button>{props.mine ? 'Mine' : 'Click'}</button>
+  <button onClick={props.clickButton}>
+    {props.clicked ? (!props.mine ? props.hint : 'x') : ''}
+  </button>
 )
 
 export default Button
--- src/Playground.js
+++ src/Playground.js
@@ -1,35 +1,96 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button from './Button'
 
 const mines = [{"x":4,"y":4},{"x":5,"y":1},{"x":4,"y":5},{"x":0,"y":0},{"x":6,"y":0},{"x":5,"y":2},{"x":8,"y":5},{"x":3,"y":1},{"x":2,"y":4},{"x":0,"y":8}]
 
 const size = 9
 
-const buttons = new Array(size).fill().map((a, i) => {
-  return new Array(size).fill({mine: false}).map((b, j) => {
-    if (mines.find(mine => mine.x === i && mine.y === j)) {
-      return {
-        ...b,
-        mine: true
-      }
+const buttons = new Array(size).fill().map((a, x) => {
+  return new Array(size).fill({mine: false, hint: 0, clicked: false}).map((b, y) => {
+    let isMine = mines.some(mine => mine.x === x && mine.y === y)
+    return {
+      ...b,
+      x,
+      y,
+      mine: isMine
     }
+  })
+})
 
-    return b
+// make hint
+mines.forEach(mine => {
+  let surroundButtons = [
+    // previous row
+    {
+      x: mine.x - 1,
+      y: mine.y - 1
+    },
+    {
+      x: mine.x - 1,
+      y: mine.y
+    },
+    {
+      x: mine.x - 1,
+      y: mine.y + 1
+    },
+    // same row
+    {
+      x: mine.x,
+      y: mine.y - 1
+    },
+    {
+      x: mine.x,
+      y: mine.y + 1
+    },
+    // next row
+    {
+      x: mine.x + 1,
+      y: mine.y - 1
+    },
+    {
+      x: mine.x + 1,
+      y: mine.y
+    },
+    {
+      x: mine.x + 1,
+      y: mine.y + 1
+    },
+  ]
+
+  surroundButtons.forEach(surroundButton => {
+    if (buttons[surroundButton.x] && buttons[surroundButton.x][surroundButton.y]) {
+      buttons[surroundButton.x][surroundButton.y].hint += 1
+    }
   })
 })
 
 console.log(buttons)
 
-const Playground = () => (
-  <div>
-    {buttons.map(row =>
-      <div className="row">
-        {row.map(button =>
-          <Button {...button}/>
-        )}
-      </div>
-    )}
-  </div>
-)
+const Playground = () => {
+  const [data, setData] = useState(buttons)
+
+  const clickButton = (button) => {
+    // skip if already clicked
+    if (button.clicked) {
+      return
+    }
+    let newData = {...data}
+    newData[button.x][button.y].clicked = true
+    setData(newData)
+  }
+
+  return (
+    <div>
+      {buttons.map(row =>
+        <div className="row">
+          {row.map(button =>
+            <Button {...button}
+              clickButton={clickButton.bind(this, button)} />
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Playground
