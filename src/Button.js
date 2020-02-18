f9343d5f296636d3952157dee643c0ff7964cdab
thanh4890 <thanh4890@gmail.com>
2020-02-18 15:32:32 +0700

    Reset game, lose game if click on mine

--- src/Button.js
+++ src/Button.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import './Button.css'
 
 const Button = props => (
-  <button onClick={props.clickButton}>
+  <button className="square" onClick={props.clickButton}>
+    &nbsp;
     {props.clicked ? (!props.mine ? props.hint : 'x') : ''}
+    &nbsp;
   </button>
 )
 
--- src/Playground.js
+++ src/Playground.js
@@ -1,87 +1,124 @@
 import React, {useState} from 'react'
 import Button from './Button'
 
-const mines = [{"x":4,"y":4},{"x":5,"y":1},{"x":4,"y":5},{"x":0,"y":0},{"x":6,"y":0},{"x":5,"y":2},{"x":8,"y":5},{"x":3,"y":1},{"x":2,"y":4},{"x":0,"y":8}]
+const makeGameData = () => {
+  const mines = [{"x":4,"y":4},{"x":5,"y":1},{"x":4,"y":5},{"x":0,"y":0},{"x":6,"y":0},{"x":5,"y":2},{"x":8,"y":5},{"x":3,"y":1},{"x":2,"y":4},{"x":0,"y":8}]
 
-const size = 9
+  const size = 9
 
-const buttons = new Array(size).fill().map((a, x) => {
-  return new Array(size).fill({mine: false, hint: 0, clicked: false}).map((b, y) => {
-    let isMine = mines.some(mine => mine.x === x && mine.y === y)
-    return {
-      ...b,
-      x,
-      y,
-      mine: isMine
-    }
+  const buttons = new Array(size).fill().map((a, x) => {
+    return new Array(size).fill({mine: false, hint: 0, clicked: false}).map((b, y) => {
+      let isMine = mines.some(mine => mine.x === x && mine.y === y)
+      return {
+        ...b,
+        x,
+        y,
+        mine: isMine
+      }
+    })
   })
-})
 
-// make hint
-mines.forEach(mine => {
-  let surroundButtons = [
-    // previous row
-    {
-      x: mine.x - 1,
-      y: mine.y - 1
-    },
-    {
-      x: mine.x - 1,
-      y: mine.y
-    },
-    {
-      x: mine.x - 1,
-      y: mine.y + 1
-    },
-    // same row
-    {
-      x: mine.x,
-      y: mine.y - 1
-    },
-    {
-      x: mine.x,
-      y: mine.y + 1
-    },
-    // next row
-    {
-      x: mine.x + 1,
-      y: mine.y - 1
-    },
-    {
-      x: mine.x + 1,
-      y: mine.y
-    },
-    {
-      x: mine.x + 1,
-      y: mine.y + 1
-    },
-  ]
+  // make hint
+  mines.forEach(mine => {
+    let surroundButtons = [
+      // previous row
+      {
+        x: mine.x - 1,
+        y: mine.y - 1
+      },
+      {
+        x: mine.x - 1,
+        y: mine.y
+      },
+      {
+        x: mine.x - 1,
+        y: mine.y + 1
+      },
+      // same row
+      {
+        x: mine.x,
+        y: mine.y - 1
+      },
+      {
+        x: mine.x,
+        y: mine.y + 1
+      },
+      // next row
+      {
+        x: mine.x + 1,
+        y: mine.y - 1
+      },
+      {
+        x: mine.x + 1,
+        y: mine.y
+      },
+      {
+        x: mine.x + 1,
+        y: mine.y + 1
+      },
+    ]
 
-  surroundButtons.forEach(surroundButton => {
-    if (buttons[surroundButton.x] && buttons[surroundButton.x][surroundButton.y]) {
-      buttons[surroundButton.x][surroundButton.y].hint += 1
-    }
+    surroundButtons.forEach(surroundButton => {
+      if (buttons[surroundButton.x] && buttons[surroundButton.x][surroundButton.y]) {
+        buttons[surroundButton.x][surroundButton.y].hint += 1
+      }
+    })
   })
-})
 
-console.log(buttons)
+  console.log(buttons)
+
+  return buttons
+}
+
+const revealAllHints = (data) => {
+  const newData = [...data]
+
+  newData.forEach(row => {
+    row.forEach(button => {
+      button.clicked = true
+    })
+  })
+
+  return newData
+}
 
 const Playground = () => {
-  const [data, setData] = useState(buttons)
+  const [status, setStatus] = useState('playing') // playing/win/lose
+  const [data, setData] = useState(makeGameData())
+
+  const resetGame = () => {
+    setStatus('playing')
+    setData(makeGameData())
+  }
+
+  const endGame = () => {
+    setData(revealAllHints(data))
+  }
 
   const clickButton = (button) => {
     // skip if already clicked
     if (button.clicked) {
       return
     }
-    let newData = {...data}
-    newData[button.x][button.y].clicked = true
-    setData(newData)
+    let newData = [...data]
+
+    // click on mine
+    if (button.mine) {
+      setStatus('lose')
+      endGame()
+      return
+
+    // not mine
+    } else {
+      newData[button.x][button.y].clicked = true
+      setData(newData)
+    }
   }
 
   return (
     <div>
-      {buttons.map(row =>
+      <button onClick={resetGame}>{status === 'lose' ? 'Cry :(' : 'Smile'}</button>
+      {data.map(row =>
         <div className="row">
           {row.map(button =>
             <Button {...button}
