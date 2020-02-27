d83a96b9f02ac6e0d7f53f8f56ab178079dc5769
thanh4890 <thanh4890@gmail.com>
2020-02-27 22:04:08 +0700

    Play with extension of jest watch files

--- src/components/Playground/__tests__/Playground.js
+++ src/components/Playground/__tests__/Playground.js
@@ -3,8 +3,8 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, mount } from 'enzyme'
 
-import Playground from './Playground'
-import EndGameModal from '../EndGameModal/EndGameModal'
+import Playground from '../Playground'
+import EndGameModal from '../../EndGameModal/EndGameModal'
 
 Enzyme.configure({ adapter: new Adapter() });
 
